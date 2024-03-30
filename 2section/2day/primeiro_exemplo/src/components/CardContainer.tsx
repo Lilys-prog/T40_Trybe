e11959f847840f61d18c6690dd8cdc41b12c0d38
Lily Silva <lilys.prog@gmail.com>
2024-03-30 19:30:53 -0300

    componentes iniciais

---
+++ 2section/2day/primeiro_exemplo/src/components/CardContainer.tsx
@@ -0,0 +1,11 @@
+const style = { backgroundColor: 'pink', color: 'purple', padding: '1rem' };
+
+function CardContainer() {
+  return (
+    <div style={ style }>
+      <p>Card Container</p>
+    </div>
+  );
+}
+
+export default CardContainer;
