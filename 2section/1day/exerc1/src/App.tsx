de6966061f183aa50fe9124277feeb6380f6f7b4
Lily Silva <lilys.prog@gmail.com>
2024-03-30 17:08:16 -0300

    exercício pronto

--- 2section/1day/exerc1/src/App.tsx
+++ 2section/1day/exerc1/src/App.tsx
@@ -1,13 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import './App.css';
+import HelloWorld from './components/HelloWorld';
 
 function App() {
-
   return (
-  
-       
-  )
+    <HelloWorld />
+  );
 }
 
-export default App
+export default App;
--- 2section/1day/exerc1/src/components/ModuleDetails.tsx
+++ 2section/1day/exerc1/src/components/ModuleDetails.tsx
@@ -1,7 +1,14 @@
 function ModuleDetails() {
   return (
-
-  )
+    <ul>
+      <li>Fundamentos</li>
+      <li>Front-end</li>
+      <li>Back-end</li>
+      <li>Ciência da Computação</li>
+      <li>Soft Skills</li>
+      <li>Carreira</li>
+    </ul>
+  );
 }
 
 export default ModuleDetails;
