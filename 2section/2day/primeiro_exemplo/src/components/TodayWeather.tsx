b84095015e72082a90a79f1e578a7ed488954744
Lily Silva <lilys.prog@gmail.com>
2024-03-31 09:18:15 -0300

    card para o clima do dia

---
+++ 2section/2day/primeiro_exemplo/src/components/TodayWeather.tsx
@@ -0,0 +1,13 @@
+function TodayWeather() {
+  return (
+    <>
+      <h2>Sunday</h2>
+      <p>Mar 31</p>
+      <img src="" alt="" />
+      <h2>Sunny</h2>
+      <p>15-20°C</p>
+    </>
+  );
+}
+
+export default TodayWeather;
